2d24c0332cb597b01fd93aef545ea672bc9048a9
MurylloEx <muryllopimenta@gmail.com>
2022-01-21 01:26:51 -0300

    :sparkles: feature: Implemented Button component.

--- src/core/components/Button/index.tsx
+++ src/core/components/Button/index.tsx
@@ -1,8 +1,16 @@
 import { Fragment, FunctionComponent } from "react";
+import { Container, Label } from "./styles";
 
-export interface ButtonProps { }
+export interface ButtonProps { 
+  text: string;
+  onClick?: () => void;
+}
 
-export const Button: FunctionComponent<ButtonProps> = () => {
-  return (<Fragment />);
+export const Button: FunctionComponent<ButtonProps> = ({ text, onClick }) => {
+  return (
+    <Container onClick={onClick ?? (() => {})}>
+      <Label>{text}</Label>
+    </Container>
+  );
 }
 
--- src/core/components/Button/styles.tsx
+++ src/core/components/Button/styles.tsx
@@ -1,1 +1,27 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  & {
+    text-align: center;
+    padding: 4px;
+    background-color: #000;
+    cursor: pointer;
+  }
+
+  &:hover {
+    opacity: 0.8;
+    transition: .4s ease 0s; 
+  }
+
+  &:active {
+    opacity: 0.6;
+    transition: .4s ease 0s; 
+  }
+`;
+
+export const Label = styled.p`
+  color: #fff;
+  user-select: none;
+  cursor: pointer;
+  font: normal normal normal 14px/1.4em din-next-w01-light,din-next-w02-light,din-next-w10-light, sans-serif;
+`;
